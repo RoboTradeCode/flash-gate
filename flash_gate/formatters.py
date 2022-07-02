57331456d8e6ce80ad995a5a907e599bacc800b8
nomnoms12 <alexander.ign0918@gmail.com>
2022-07-02 20:10:52 +0300

    Refactoring

--- flash_gate/formatters.py
+++ flash_gate/formatters.py
@@ -11,21 +11,22 @@ class Formatter:
         self.algo = algo
 
     def format(self, event_id: str, event: Event, action: Action, data) -> str:
-        return json.dumps(
-            {
-                "event_id": event_id,
-                "event": event,
-                "exchange": self.exchange,
-                "node": self.node,
-                "instance": self.instance,
-                "algo": self.algo,
-                "action": action,
-                "message": "",
-                "timestamp": self._get_timestamp_in_us(),
-                "data": data,
-            }
-        )
+        deserialized_message = {
+            "event_id": event_id,
+            "event": event,
+            "exchange": self.exchange,
+            "node": self.node,
+            "instance": self.instance,
+            "algo": self.algo,
+            "action": action,
+            "message": "",
+            "timestamp": self._get_timestamp_in_us(),
+            "data": data,
+        }
+
+        message = json.dumps(deserialized_message)
+        return message
 
     @staticmethod
-    def _get_timestamp_in_us():
+    def _get_timestamp_in_us() -> int:
         return int(datetime.now().timestamp() * 1000000)
